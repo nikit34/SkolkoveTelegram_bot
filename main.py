0e5914b20edff3e30795e7e8b8a1130244e80ca8
nikit34 <permikov134@mail.ru>
2021-03-20 15:52:45 +0300

    separate

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
-from telegram.ext import Updater, CommandHandler
 import os
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+from .actions import *
 
 
 updater = Updater(token=os.environ['TOKEN_BOT'], use_context=True)
-
-
 dispatcher = updater.dispatcher
 
-import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
-
-
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot!")
-
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
 updater.start_polling()
+
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
